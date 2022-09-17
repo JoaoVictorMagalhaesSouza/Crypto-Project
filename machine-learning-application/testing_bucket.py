d511ed9a2398cf3c36dec2dd9aa8208fc7cb5593
João <JoaoVictorMagalhaesSouza@gmail.com>
2022-09-17 11:30:45 -0300

    Replace figure

--- machine-learning-application/testing_bucket.py
+++ machine-learning-application/testing_bucket.py
@@ -1,52 +1,63 @@
 from google.cloud import storage
 import json
-config_data = json.load(open('configs.json'))
-
-def authenticate_implicit_with_adc(project_id=config_data['project_id']):
-    """
-    When interacting with Google Cloud Client libraries, the library can auto-detect the
-    credentials to use.
-
-    // TODO(Developer):
-    //  1. Before running this sample,
-    //  set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
-    //  2. Replace the project variable.
-    //  3. Make sure that the user account or service account that you are using
-    //  has the required permissions. For this sample, you must have "storage.buckets.list".
-    Args:
-        project_id: The project id of your Google Cloud project.
-    """
-
-    # This snippet demonstrates how to list buckets.
-    # *NOTE*: Replace the client created below with the client required for your application.
-    # Note that the credentials are not specified when constructing the client.
-    # Hence, the client library will look for credentials using ADC.
-    storage_client = storage.Client(project=project_id)
-    buckets = storage_client.list_buckets()
-    print("Buckets:")
-    for bucket in buckets:
-        print(bucket.name)
-    print("Listed all storage buckets.")
 
 
-def upload_blob(source_file_name, bucket_name = config_data['bucket_name'], destination_blob_name = config_data['destination_blob_name']):
-    """Uploads a file to the bucket."""
+class BucketArchitecture():
 
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
+    def __init__(self):
+        self.config_data = json.load(open('configs.json'))
 
-    # The path file to upload
-    # contents = "requirements.txt"
+    def authenticate_implicit_with_adc(self):
+        """
+        When interacting with Google Cloud Client libraries, the library can auto-detect the
+        credentials to use.
 
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
+        // TODO(Developer):
+        //  1. Before running this sample,
+        //  set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+        //  2. Replace the project variable.
+        //  3. Make sure that the user account or service account that you are using
+        //  has the required permissions. For this sample, you must have "storage.buckets.list".
+        Args:
+            project_id: The project id of your Google Cloud project.
+        """
+        project_id = self.config_data['project_id']
+        # This snippet demonstrates how to list buckets.
+        # *NOTE*: Replace the client created below with the client required for your application.
+        # Note that the credentials are not specified when constructing the client.
+        # Hence, the client library will look for credentials using ADC.
+        storage_client = storage.Client(project=project_id)
+        buckets = storage_client.list_buckets()
+        print("Buckets:")
+        for bucket in buckets:
+            print(bucket.name)
+        print("Listed all storage buckets.")
 
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_filename(source_file_name)
+    def upload_blob(self,source_file_name):
+        """Uploads a file to the bucket."""
 
-    print(
-        f"{destination_blob_name} with path {source_file_name} uploaded to {bucket_name}."
-    )
+        # The ID of your GCS bucket
+        # bucket_name = "your-bucket-name"
+
+        # The path file to upload
+        # contents = "requirements.txt"
+
+        # The ID of your GCS object
+        # destination_blob_name = "storage-object-name"
+        bucket_name = self.config_data['bucket_name']
+        destination_blob_name = self.config_data['destination_blob_name']
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_file_name)
+
+        print(
+            f"{destination_blob_name} with path {source_file_name} uploaded to {bucket_name}."
+        )
+    
+    def save_models_in_bucket(self, source_file_name):
+        self.authenticate_implicit_with_adc()
+        self.upload_blob(source_file_name)
