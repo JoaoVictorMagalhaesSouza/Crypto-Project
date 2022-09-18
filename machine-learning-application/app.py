ec6e750cf6a0a30e152fbc264dcaf32c0668137d
João <JoaoVictorMagalhaesSouza@gmail.com>
2022-09-18 20:33:12 -0300

    Creating app for ML

--- machine-learning-application/app.py
+++ machine-learning-application/app.py
@@ -1,3 +1,17 @@
 from flask import Flask
 from pipelines import real_time_predict_pipeline, real_time_train_pipeline
 
+app = Flask(__name__)
+
+@app.route('/train')
+def train_model():
+    real_time_train_pipeline.RealTimeTrain().train_pipeline()
+    return "Train sucessfull !"
+
+@app.route('/predict')
+def predict():
+    real_time_predict_pipeline.RealTimePredict().predict_pipeline()
+    return "Predict sucessfull !"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
