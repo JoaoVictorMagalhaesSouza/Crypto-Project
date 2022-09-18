af188349b5c989b0d429fd6e0e68824ab085bf7f
João <JoaoVictorMagalhaesSouza@gmail.com>
2022-09-18 12:13:45 -0300

    Add DB structure to save data from training models

--- machine-learning-application/pipelines/real_time_train_pipeline.py
+++ machine-learning-application/pipelines/real_time_train_pipeline.py
@@ -1,6 +1,10 @@
 
 import sys
 import joblib
+import json
+import psycopg2
+import pandas as pd
+from datetime import datetime, timedelta
 sys.path.append('../')
 
 from utils import data_acquisition as da
@@ -13,6 +17,31 @@ class RealTimeTrain():
     def __init__(self, list_cryptos_to_train = af.get_all_available_cryptos()['db_crypto_name'].values):
         self.to_train = list_cryptos_to_train
     
+    def save_data_into_database(self,crypto, model_name):
+        config_data = json.load(open('../configs.json'))
+        conn = psycopg2.connect(
+            host = config_data['host'], 
+            database = config_data['database'],
+            user = config_data['user'],
+            password = config_data['password']
+            )
+        cursor = conn.cursor()
+        query = f'SELECT * FROM models'
+        data = pd.read_sql(query,conn)
+        cryptos = data['crypto'].values
+        now = (datetime.now()-timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')
+        
+        if crypto in cryptos:
+            query = f"UPDATE models SET last_train = '{now}' WHERE (crypto = '{crypto}')"
+            cursor.execute(query)
+
+        else:
+            query = f"INSERT INTO models (crypto, model_name, last_train) VALUES ('{crypto}','{model_name}','{now}')"
+            cursor.execute(query)
+        
+        cursor.close()
+        conn.commit()
+
     def train_pipeline(self):
         for crypto in self.to_train:
             print(f"Training {crypto} model...")
@@ -22,10 +51,11 @@ class RealTimeTrain():
 
             #Here we go to apply optuna to decide best model for wich crypto...
             model = arch.XGBModel()
+            type = 'xgb'
             model.init_model()
             model.fit(X_train, y_train)
             ###
             
-            filename = f"../saved_models/{crypto}_model.joblib"
+            filename = f"../saved_models/{crypto}_model_{type}.joblib"
             joblib.dump(model,filename)
-            print(" => OK")
+            self.save_data_into_database(crypto,f'{crypto}_model_{type}.joblib')
